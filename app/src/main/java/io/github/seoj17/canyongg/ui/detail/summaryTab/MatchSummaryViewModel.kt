feb320fe20a19f6ba3c2066e1680efa38e7547e7
seojeongwoo <qw4643987@naver.com>
2023-03-12 22:02:40 +0900

    #10 :hammer: 데이터 흐름으로 되도록 코드 수정함

--- app/src/main/java/io/github/seoj17/canyongg/ui/detail/summaryTab/MatchSummaryViewModel.kt
+++ app/src/main/java/io/github/seoj17/canyongg/ui/detail/summaryTab/MatchSummaryViewModel.kt
@@ -3,13 +3,13 @@ package io.github.seoj17.canyongg.ui.detail.summaryTab
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
 import androidx.lifecycle.map
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.switchMap
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.github.seoj17.canyongg.domain.usecase.match.GetParticipantsMatchesUseCase
 import io.github.seoj17.canyongg.ui.model.SummonerMatchRecord
 import io.github.seoj17.canyongg.ui.model.TeamKdaInfo
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -20,14 +20,21 @@ class MatchSummaryViewModel @Inject constructor(
     private val _matchId = MutableLiveData<String>()
     val matchId: LiveData<String> = _matchId
 
-    private val _participantsMatches = MutableLiveData<List<SummonerMatchRecord>>(emptyList())
-    val participantsMatches: LiveData<List<SummonerMatchRecord>> = _participantsMatches
+    val participantsMatches = matchId.switchMap { matchId ->
+        liveData {
+            emit(
+                getParticipantsMatches(matchId).map {
+                    SummonerMatchRecord(it)
+                }
+            )
+        }
+    }
 
-    val winTeam: LiveData<List<SummonerMatchRecord>> = _participantsMatches.map { matches ->
+    val winTeam: LiveData<List<SummonerMatchRecord>> = participantsMatches.map { matches ->
         matches.filter { it.win }
     }
 
-    val loseTeam: LiveData<List<SummonerMatchRecord>> = _participantsMatches.map { matches ->
+    val loseTeam: LiveData<List<SummonerMatchRecord>> = participantsMatches.map { matches ->
         matches.filter { !it.win }
     }
 
@@ -37,16 +44,6 @@ class MatchSummaryViewModel @Inject constructor(
 
     val loseTeamKdaInfo = loseTeam.map { matchList ->
         TeamKdaInfo.toKdaInfo(matchList, false)
-    }
-
-    fun fetch() {
-        _matchId.value?.let { matchId ->
-            viewModelScope.launch {
-                _participantsMatches.value = getParticipantsMatches(matchId).map {
-                    SummonerMatchRecord(it)
-                }
-            }
-        }
     }
 
     fun setMatchId(matchId: String) {
