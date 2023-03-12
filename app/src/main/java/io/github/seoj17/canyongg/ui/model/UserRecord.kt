8e251027d94406790a7054fbc1fd7079461e0599
SEO-J17 <qw4643987@naver.com>
2023-03-12 23:15:53 +0900

    #10 :hammer: 모델의 default 값 삭제함

--- app/src/main/java/io/github/seoj17/canyongg/ui/model/UserRecord.kt
+++ app/src/main/java/io/github/seoj17/canyongg/ui/model/UserRecord.kt
@@ -3,12 +3,12 @@ package io.github.seoj17.canyongg.ui.model
 import io.github.seoj17.canyongg.domain.model.UserRecordDomainModel
 
 data class UserRecord(
-    val wholeMatch: Int = 0,
-    val winCount: Int = 0,
-    val loseCount: Int = 0,
-    val winRate: Int = 0,
-    val kda: Double = 0.0,
-    val largestKill: Int = 0,
+    val wholeMatch: Int,
+    val winCount: Int,
+    val loseCount: Int,
+    val winRate: Int,
+    val kda: Double,
+    val largestKill: Int,
 ) {
     companion object {
         operator fun invoke(domain: UserRecordDomainModel): UserRecord {
