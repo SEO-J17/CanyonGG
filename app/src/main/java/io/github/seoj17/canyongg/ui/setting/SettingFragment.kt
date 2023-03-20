09f28fa3c70f767964648bc7d0d7809bc1beee6d
SEO-J17 <qw4643987@naver.com>
2023-03-20 22:41:32 +0900

    #8 :hammer: 설정 화면 로그인 클릭리스너 추가함

--- app/src/main/java/io/github/seoj17/canyongg/ui/setting/SettingFragment.kt
+++ app/src/main/java/io/github/seoj17/canyongg/ui/setting/SettingFragment.kt
@@ -5,10 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import dagger.hilt.android.AndroidEntryPoint
 import io.github.seoj17.canyongg.databinding.FragmentSettingBinding
 
+@AndroidEntryPoint
 class SettingFragment : Fragment() {
     private lateinit var binding: FragmentSettingBinding
+    private val viewModel: SettingViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -17,5 +22,19 @@ class SettingFragment : Fragment() {
         super.onCreateView(inflater, container, savedInstanceState)
         binding = FragmentSettingBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+            lifecycleOwner = viewLifecycleOwner
+            vm = viewModel
+
+            login.setOnClickListener {
+                findNavController().navigate(
+                    SettingFragmentDirections.actionSettingToLoginFragment(),
+                )
+            }
+        }
     }
 }
