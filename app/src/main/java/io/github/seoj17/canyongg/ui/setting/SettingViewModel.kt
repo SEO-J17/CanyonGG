c5478bc5815ca50faf625c40467815e575706fbb
SEO-J17 <qw4643987@naver.com>
2023-03-21 20:23:36 +0900

    #13 :hammer: dataStore에 테마 상태 add하는 코드 구현함

--- app/src/main/java/io/github/seoj17/canyongg/ui/setting/SettingViewModel.kt
+++ app/src/main/java/io/github/seoj17/canyongg/ui/setting/SettingViewModel.kt
@@ -3,19 +3,29 @@ package io.github.seoj17.canyongg.ui.setting
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.github.seoj17.canyongg.domain.usecase.setting.AddThemeSettingUseCase
+import io.github.seoj17.canyongg.domain.usecase.setting.GetThemeSettingUseCase
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class SettingViewModel @Inject constructor() : ViewModel() {
+class SettingViewModel @Inject constructor(
+    private val addThemeSettingUseCase: AddThemeSettingUseCase,
+    private val getThemeSettingUseCase: GetThemeSettingUseCase,
+) : ViewModel() {
 
     private val _loginState = MutableLiveData<Boolean>()
     val loginState: LiveData<Boolean> = _loginState
 
     private val _currentEmail = MutableLiveData<String>()
     val currentEmail: LiveData<String> = _currentEmail
+
+    val themeSetting: LiveData<Int?> = getThemeSettingUseCase(ThemeState.THEME.key).asLiveData()
 
     init {
         Firebase
@@ -33,5 +43,11 @@ class SettingViewModel @Inject constructor() : ViewModel() {
     fun userLogout() {
         Firebase.auth.signOut()
         _loginState.value = false
+    }
+
+    fun fetchThemeSetting(id: Int) {
+        viewModelScope.launch {
+            addThemeSettingUseCase(ThemeState.THEME.key, id)
+        }
     }
 }
