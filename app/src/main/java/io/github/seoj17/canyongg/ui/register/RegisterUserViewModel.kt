ca951b71b821b54fe328120bc1fff6bebcf00571
SEO-J17 <qw4643987@naver.com>
2023-03-27 19:54:54 +0900

    #15 :hammer: 뷰모델 코드 정리함

--- app/src/main/java/io/github/seoj17/canyongg/ui/register/RegisterUserViewModel.kt
+++ app/src/main/java/io/github/seoj17/canyongg/ui/register/RegisterUserViewModel.kt
@@ -1,46 +1,72 @@
 package io.github.seoj17.canyongg.ui.register
 
+import android.util.Patterns
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asFlow
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.map
+import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class RegisterUserViewModel @Inject constructor() : ViewModel() {
 
-    private var emailValid = false
+    val email = MutableLiveData<String>()
+    val password = MutableLiveData<String>()
+    val passwordChk = MutableLiveData<String>()
 
-    private var passwordValid = false
+    val emailValid = email.map {
+        if (it.isBlank()) return@map false
 
-    private val _registerEvent = MutableLiveData<Boolean>()
-    val registerEvent: LiveData<Boolean> = _registerEvent
+        return@map Patterns
+            .EMAIL_ADDRESS
+            .matcher(it.toString())
+            .matches()
+    }
 
-    private val _validInfo = MutableLiveData<Boolean>()
-    val validInfo: LiveData<Boolean> = _validInfo
+    val passwordValid: LiveData<Boolean> = password.map {
+        return@map it.length > 5
+    }
 
-    fun registerSubmit(email: String, password: String) {
+    val passwordChkValid: LiveData<Boolean> = passwordChk.map {
+        return@map it == password.value
+    }
+
+    val validLoginInfo =
+        combine(
+            emailValid.asFlow(),
+            passwordValid.asFlow(),
+            passwordChkValid.asFlow(),
+        ) { emailValid, passwordValid, passwordChkValid ->
+            (emailValid && passwordValid && passwordChkValid)
+        }
+            .asLiveData()
+
+    private val _registerState = MutableSharedFlow<RegisterState>()
+    val registerState: SharedFlow<RegisterState> = _registerState.asSharedFlow()
+
+    fun registerSubmit() {
         Firebase
             .auth
-            .createUserWithEmailAndPassword(email, password)
+            .createUserWithEmailAndPassword(email.value!!, password.value!!)
             .addOnCompleteListener { task ->
-                _registerEvent.value = task.isSuccessful
+                viewModelScope.launch {
+                    if (task.isSuccessful) {
+                        _registerState.emit(RegisterState.SUCCESS)
+                    } else {
+                        _registerState.emit(RegisterState.FAIL)
+                    }
+                }
             }
-    }
-
-    fun fetchEmailValid(state: Boolean) {
-        emailValid = state
-        validInfo()
-    }
-
-    fun fetchPasswordValid(state: Boolean) {
-        passwordValid = state
-        validInfo()
-    }
-
-    private fun validInfo() {
-        _validInfo.value = (emailValid && passwordValid)
     }
 }
