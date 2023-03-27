9a271b085a119c96000fe955efc617d256ea5d51
SEO-J17 <qw4643987@naver.com>
2023-03-27 19:53:42 +0900

    #15 :hammer: flow받는 코드 적용함

--- app/src/main/java/io/github/seoj17/canyongg/ui/login/LoginFragment.kt
+++ app/src/main/java/io/github/seoj17/canyongg/ui/login/LoginFragment.kt
@@ -1,18 +1,20 @@
 package io.github.seoj17.canyongg.ui.login
 
 import android.os.Bundle
-import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import io.github.seoj17.canyongg.R
 import io.github.seoj17.canyongg.databinding.FragmentLoginBinding
+import io.github.seoj17.canyongg.utils.showToast
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class LoginFragment : Fragment() {
@@ -30,50 +32,33 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         with(binding) {
             vm = viewModel
             lifecycleOwner = viewLifecycleOwner
-
-            email.addTextChangedListener {
-                Patterns
-                    .EMAIL_ADDRESS
-                    .matcher(it.toString())
-                    .run {
-                        if (matches()) {
-                            emailLayout.error = null
-                            viewModel.fetchEmailValid(true)
-                        } else {
-                            emailLayout.error = getString(R.string.error_email_type)
-                            viewModel.fetchEmailValid(false)
-                        }
-                    }
-            }
-
-            password.addTextChangedListener {
-                if (it.isNullOrBlank()) {
-                    passwordLayout.error = getString(R.string.login_password_message)
-                    viewModel.fetchPasswordValid(false)
-                } else {
-                    passwordLayout.error = null
-                    viewModel.fetchPasswordValid(true)
-                }
-            }
-
-            viewModel.loginEvent.observe(viewLifecycleOwner) {
-                if (it) {
-                    Toast.makeText(context, R.string.success_login, Toast.LENGTH_SHORT).show()
-                    findNavController().navigate(
-                        LoginFragmentDirections.actionLoginFragmentToSetting(),
-                    )
-                } else {
-                    Toast.makeText(context, R.string.fail_login, Toast.LENGTH_SHORT).show()
-                }
-            }
 
             register.setOnClickListener {
                 findNavController().navigate(
                     LoginFragmentDirections.actionLoginFragmentToRegisterUserFragment(),
                 )
+            }
+        }
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                viewModel.loginState.collect { state ->
+                    when (state) {
+                        is LoginState.SUCCESS -> {
+                            requireActivity().showToast(R.string.success_login)
+                            findNavController().navigate(
+                                LoginFragmentDirections.actionLoginFragmentToSetting(),
+                            )
+                        }
+                        is LoginState.FAIL -> {
+                            requireActivity().showToast(R.string.fail_login)
+                        }
+                    }
+                }
             }
         }
     }
