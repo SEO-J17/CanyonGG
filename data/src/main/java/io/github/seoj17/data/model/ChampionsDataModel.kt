b2498c6d15ff9926cd20cf6a036ab9bcb6925d51
SEO JEONGWOO <59912150+SEO-J17@users.noreply.github.com>
2023-04-05 01:32:57 +0900

    🐛 #25 제일 처음 실행 할 때 로테이션 챔피언 나타나지 않는 오류  (#27)
    * #25 :hammer: nullable로 리턴 타입 수정함
    
    * #25 :hammer: 엔티티에서 데이터 모델로 변경해주는 함수 추가함
    
    * #25 :hammer: 리턴타입 변경된 것으로 코드 적용함
    
    * #25 :hammer: null 처리하는 코드로 수정함
    
    * #25 :truck: 유스케이스 이름 변경함
    
    * #25 :hammer: delay코드 삭제, 코드 수정함

--- data/src/main/java/io/github/seoj17/data/model/ChampionsDataModel.kt
+++ data/src/main/java/io/github/seoj17/data/model/ChampionsDataModel.kt
@@ -1,6 +1,17 @@
 package io.github.seoj17.data.model
 
+import io.github.seoj17.data.local.champions.ChampionsEntity
+
 data class ChampionsDataModel(
     val key: Int,
     val name: String,
-)
+) {
+    companion object {
+        operator fun invoke(entity: ChampionsEntity): ChampionsDataModel {
+            return ChampionsDataModel(
+                key = entity.key,
+                name = entity.name,
+            )
+        }
+    }
+}
