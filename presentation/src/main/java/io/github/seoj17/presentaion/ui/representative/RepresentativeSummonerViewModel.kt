0aaf5f15bf2a0a6ea2527df4dc6bb553f9ab6ebf
SEO-J17 <qw4643987@naver.com>
2023-04-17 22:38:27 +0900

    #38 :hammer: 에러 이벤트 코드 삭제함

--- presentation/src/main/java/io/github/seoj17/presentaion/ui/representative/RepresentativeSummonerViewModel.kt
+++ presentation/src/main/java/io/github/seoj17/presentaion/ui/representative/RepresentativeSummonerViewModel.kt
@@ -7,7 +7,6 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.github.seoj17.domain.usecase.user.GetUserInfoUseCase
 import io.github.seoj17.presentaion.model.Summoner
-import io.github.seoj17.presentaion.utils.Event
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -16,21 +15,16 @@ class RepresentativeSummonerViewModel @Inject constructor(
     private val getUserInfoUseCase: GetUserInfoUseCase,
 ) : ViewModel() {
 
-    val summonerName = MutableLiveData<String>("")
+    val summonerName = MutableLiveData<String>()
 
     private val _searchResult = MutableLiveData<Summoner?>()
     val searchResult: LiveData<Summoner?> = _searchResult
 
-    private val _errorEvent = MutableLiveData<Event<Boolean>>()
-    val errorEvent: LiveData<Event<Boolean>> = _errorEvent
-
     fun searchSummoner() {
         viewModelScope.launch {
-            getUserInfoUseCase(summonerName.value!!)?.let { summonerDomain ->
+            getUserInfoUseCase(summonerName.value)?.let { summonerDomain ->
                 val summoner = Summoner(summonerDomain)
                 _searchResult.value = summoner
-            } ?: run {
-                _errorEvent.value = Event(true)
             }
         }
     }
