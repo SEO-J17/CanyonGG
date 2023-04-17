9756494ee85fe760ce9a640d488e01ab7b7fe629
SEO-J17 <qw4643987@naver.com>
2023-04-17 22:40:33 +0900

    #38 :hammer: 모스트 챔피언 관련 함수 분리함

--- data/src/main/java/io/github/seoj17/data/repository/RegisterUserRepository.kt
+++ data/src/main/java/io/github/seoj17/data/repository/RegisterUserRepository.kt
@@ -1,16 +1,11 @@
 package io.github.seoj17.data.repository
 
 import io.github.seoj17.data.local.user.RegisterUserInfoEntity
-import io.github.seoj17.data.local.user.RegisterUserMostChampEntity
-import io.github.seoj17.data.model.MostChampionDataModel
 import io.github.seoj17.data.model.RegisterUserDataModel
 import kotlinx.coroutines.flow.Flow
 
 interface RegisterUserRepository {
     fun getMyUserInfo(): Flow<RegisterUserDataModel?>
-    fun getMyMostChamps(): Flow<List<MostChampionDataModel>>
     suspend fun addMyUserInfo(entity: RegisterUserInfoEntity)
-    suspend fun addMostChamps(entity: List<RegisterUserMostChampEntity>)
-    suspend fun deleteMostChamps()
     suspend fun deleteMyUserChamps()
 }
