9fd4102c8b362ccf403d4f6441ebb3e9470dd59f
SEO-J17 <qw4643987@naver.com>
2023-04-29 01:21:49 +0900

    #5 :hammer: 0일때 예외 수정

--- domain/src/main/java/io/github/seoj17/domain/usecase/user/GetUserRecordUseCase.kt
+++ domain/src/main/java/io/github/seoj17/domain/usecase/user/GetUserRecordUseCase.kt
@@ -2,12 +2,12 @@ package io.github.seoj17.domain.usecase.user
 
 import dagger.Reusable
 import io.github.seoj17.domain.model.UserRecordDomainModel
-import io.github.seoj17.domain.usecase.match.GetRegisterUserMatchListUseCase
+import io.github.seoj17.domain.usecase.match.GetRepresentativeUserMatchListUseCase
 import javax.inject.Inject
 
 @Reusable
 class GetUserRecordUseCase @Inject constructor(
-    private val getRegisterUserMatchListUseCase: GetRegisterUserMatchListUseCase,
+    private val getRegisterUserMatchListUseCase: GetRepresentativeUserMatchListUseCase,
 ) {
     suspend operator fun invoke(puuid: String): UserRecordDomainModel {
         getRegisterUserMatchListUseCase(puuid)
@@ -19,7 +19,7 @@ class GetUserRecordUseCase @Inject constructor(
 
                 val win = count { it.win && !it.gameEndedInEarlySurrender }
                 val lose = realMatch - win
-                val winRate = (win * 100) / realMatch
+                val winRate = (win * 100) / if (realMatch == 0) 1 else realMatch
                 val kda = kills / sumOf { it.deaths }.toDouble()
                 val mostKill = maxOf { it.largestKill }
 
