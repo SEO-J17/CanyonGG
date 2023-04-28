c9451722745ff6125603ee382f09eb675fdb81b1
SEO-J17 <qw4643987@naver.com>
2023-04-29 01:25:03 +0900

    #5 :hammer: UI 상태 나타내는 flow 코드 추가

--- presentation/src/main/java/io/github/seoj17/presentaion/ui/record/SummonerRecordViewModel.kt
+++ presentation/src/main/java/io/github/seoj17/presentaion/ui/record/SummonerRecordViewModel.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
-import androidx.lifecycle.map
+import androidx.lifecycle.liveData
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
 import androidx.paging.map
@@ -16,7 +16,6 @@ import io.github.seoj17.domain.usecase.bookmark.DeleteBookmarkSummonerUseCase
 import io.github.seoj17.domain.usecase.summoner.AddSummonerInfoUseCase
 import io.github.seoj17.domain.usecase.summoner.GetSummonerHistoryUseCase
 import io.github.seoj17.domain.usecase.summoner.GetSummonerInfoUseCase
-import io.github.seoj17.domain.usecase.user.GetUserInfoUseCase
 import io.github.seoj17.domain.usecase.user.GetUserRecordUseCase
 import io.github.seoj17.domain.usecase.user.GetUserTierUseCase
 import io.github.seoj17.presentaion.model.Summoner
@@ -24,13 +23,16 @@ import io.github.seoj17.presentaion.model.SummonerBookmark
 import io.github.seoj17.presentaion.model.SummonerInfo
 import io.github.seoj17.presentaion.model.SummonerMatchRecord
 import io.github.seoj17.presentaion.model.UserRecord
+import io.github.seoj17.presentaion.ui.state.UiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SummonerRecordViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
-    private val getUserInfoUseCase: GetUserInfoUseCase,
     private val getUserTierUseCase: GetUserTierUseCase,
     getSummonerHistoryUseCase: GetSummonerHistoryUseCase,
     private val addBookmarkSummoner: AddBookmarkSummonerUseCase,
@@ -47,40 +49,45 @@ class SummonerRecordViewModel @Inject constructor(
     private val summonerPuuid =
         SummonerRecordFragmentArgs.fromSavedStateHandle(savedStateHandle).summonerPuuid
 
-    val summonerInfo = getSummonerInfoUseCase(summonerPuuid)
-        .asLiveData()
-        .map { domain ->
-            domain?.let { it -> SummonerInfo(it) }
+    private val _searchState = MutableStateFlow<UiState>(UiState.Empty)
+    val searchState: StateFlow<UiState> = _searchState.asStateFlow()
+
+    val summonerInfo = liveData {
+        getSummonerInfoUseCase(summonerPuuid).collect { domain ->
+            domain?.let {
+                _summonerMostKill.value = it.largestKill
+                emit(SummonerInfo(it))
+            }
         }
+    }
 
     private val _summonerMostKill = MutableLiveData<Int>()
     val summonerMostKill: LiveData<Int> = _summonerMostKill
 
-    val summonerRecordHistory = getSummonerHistoryUseCase(summonerPuuid)
-        .asLiveData()
-        .map { paging ->
-            paging.map {
-                SummonerMatchRecord(it)
-            }
+    val summonerRecordHistory = liveData {
+        _searchState.emit(UiState.Loading)
+        getSummonerHistoryUseCase(summonerPuuid).collect { paging ->
+            emit(
+                paging.map {
+                    _searchState.emit(UiState.Success)
+                    SummonerMatchRecord(it)
+                },
+            )
         }
-        .cachedIn(viewModelScope)
+    }.cachedIn(viewModelScope)
 
     val bookmarkedSummoner = checkBookmarkedSummoner(summonerPuuid).asLiveData()
 
-    init {
+    fun fetchSummonerInfo(summoner: Summoner) {
         viewModelScope.launch {
-            getUserInfoUseCase(summonerName)?.let { summonerDomain ->
-                val summoner = Summoner(summonerDomain)
+            val tier = getUserTierUseCase(summoner.name)?.let { summonerTier ->
+                "${summonerTier.tier} ${summonerTier.rank}"
+            } ?: "UNRANKED"
 
-                val tier = getUserTierUseCase(summoner.id)?.let { summonerTier ->
-                    "${summonerTier.tier} ${summonerTier.rank}"
-                } ?: "UNRANKED"
+            val userRecord = UserRecord(getUserRecordUseCase(summoner.puuid))
+            _summonerMostKill.value = userRecord.largestKill
 
-                val userRecord = UserRecord(getUserRecordUseCase(summoner.puuid))
-                _summonerMostKill.value = userRecord.largestKill
-
-                insertSummonerInfoLocal(userRecord, summoner, tier)
-            }
+            insertSummonerInfoLocal(userRecord, summoner, tier)
         }
     }
 
