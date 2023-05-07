7cbd420b9e495da22b1dbb091939bdd942c1e4fc
SEO-J17 <qw4643987@naver.com>
2023-05-08 02:38:53 +0900

    #20 :sparkles: 베이스 프래그먼트 생성함

---
+++ presentation/src/main/java/io/github/seoj17/presentaion/ui/base/BaseFragment.kt
@@ -0,0 +1,44 @@
+package io.github.seoj17.presentaion.ui.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+
+abstract class BaseFragment<VB : ViewDataBinding, VM : ViewModel>(
+    private val inflater: (LayoutInflater, ViewGroup?, Boolean) -> VB,
+) : Fragment() {
+
+    private var _binding: VB? = null
+    val binding
+        get() = requireNotNull(_binding)
+
+    abstract val viewModel: VM
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        _binding = inflater(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            lifecycleOwner = viewLifecycleOwner
+        }
+
+        bindLayout()
+        observeViewModel()
+    }
+
+    abstract fun bindLayout()
+    abstract fun observeViewModel()
+}
