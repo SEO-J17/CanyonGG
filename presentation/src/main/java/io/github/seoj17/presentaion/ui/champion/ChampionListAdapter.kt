39cfd042f12649fb05cc37ab407f2f793c7c57d5
SEO-J17 <qw4643987@naver.com>
2023-05-09 01:51:20 +0900

    #50 :sparkles: 챔피언 리사이클러뷰 구현함

---
+++ presentation/src/main/java/io/github/seoj17/presentaion/ui/champion/ChampionListAdapter.kt
@@ -0,0 +1,18 @@
+package io.github.seoj17.presentaion.ui.champion
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import io.github.seoj17.presentaion.model.Champion
+
+class ChampionListAdapter(
+    private val onChampionClick: () -> Unit,
+) : ListAdapter<Champion, ChampionViewHolder>(Champion.diffUtil) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ChampionViewHolder(parent)
+
+    override fun onBindViewHolder(holder: ChampionViewHolder, position: Int) {
+        val data = getItem(position)
+        with(holder) {
+            bind(data)
+        }
+    }
+}
