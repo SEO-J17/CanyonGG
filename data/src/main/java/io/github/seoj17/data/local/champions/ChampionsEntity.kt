222b7ee58106cf7dd2d9c8c347d423dbe31de1ce
SEO-J17 <qw4643987@naver.com>
2023-05-09 01:52:11 +0900

    #50 :sparkles: 챔피언 엔티티에 데이터 추가함

--- data/src/main/java/io/github/seoj17/data/local/champions/ChampionsEntity.kt
+++ data/src/main/java/io/github/seoj17/data/local/champions/ChampionsEntity.kt
@@ -9,6 +9,32 @@ data class ChampionsEntity(
     @PrimaryKey
     @ColumnInfo(name = "champion_key")
     val key: Int,
-    @ColumnInfo(name = "champion_name")
+    @ColumnInfo(name = "champion_id")
+    val id: String,
+    @ColumnInfo(name = "name")
     val name: String,
+    @ColumnInfo(name = "title")
+    val title: String,
+    @ColumnInfo(name = "tag")
+    val tag: String,
+    @ColumnInfo(name = "blurb")
+    val blurb: String,
+    @ColumnInfo(name = "hp")
+    val hp: Int,
+    @ColumnInfo(name = "hp_per_level")
+    val hpPerLevel: Int,
+    @ColumnInfo(name = "move_speed")
+    val moveSpeed: Int,
+    @ColumnInfo(name = "attack_damage")
+    val attackDamage: Int,
+    @ColumnInfo(name = "attack_damage_perLevel")
+    val attackDamagePerLevel: Double,
+    @ColumnInfo(name = "armor")
+    val armor: Int,
+    @ColumnInfo(name = "armor_per_level")
+    val armorPerLevel: Double,
+    @ColumnInfo(name = "spell_block")
+    val spellBlock: Int,
+    @ColumnInfo(name = "spell_block_per_level")
+    val spellBlockPerLevel: Double,
 )
