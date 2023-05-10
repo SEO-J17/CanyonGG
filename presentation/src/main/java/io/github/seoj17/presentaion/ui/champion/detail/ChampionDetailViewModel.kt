77b26bfcd987abf0212f143c21a5cb63332bfb4a
SEO-J17 <qw4643987@naver.com>
2023-05-11 04:03:15 +0900

    #53 :hammer: champion 디테일 뷰모델 구현함

--- presentation/src/main/java/io/github/seoj17/presentaion/ui/champion/detail/ChampionDetailViewModel.kt
+++ presentation/src/main/java/io/github/seoj17/presentaion/ui/champion/detail/ChampionDetailViewModel.kt
@@ -1,7 +1,33 @@
 package io.github.seoj17.presentaion.ui.champion.detail
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.github.seoj17.domain.usecase.champion.GetChampionDetailUseCase
+import io.github.seoj17.presentaion.model.Champion
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class ChampionDetailViewModel @Inject constructor() : ViewModel() {
+@HiltViewModel
+class ChampionDetailViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
+    private val getChampionDetailUseCase: GetChampionDetailUseCase,
+) : ViewModel() {
+
+    private val championKey =
+        ChampionDetailFragmentArgs.fromSavedStateHandle(savedStateHandle).champKey
+
+    private val _champDetail = MutableLiveData<Champion>()
+    val champDetail: LiveData<Champion> = _champDetail
+
+    init {
+        viewModelScope.launch {
+            getChampionDetailUseCase(championKey)?.let {
+                _champDetail.value = Champion(it)
+            }
+        }
+    }
 }
