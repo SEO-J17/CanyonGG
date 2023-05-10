2c27dd45ec4f6d200d628fcb394c79471a20dfde
SEO-J17 <qw4643987@naver.com>
2023-05-11 02:13:14 +0900

    #52 :sparkles: 챔피언 상세 뷰모델 생성함

---
+++ presentation/src/main/java/io/github/seoj17/presentaion/ui/champion/detail/ChampionDetailViewModel.kt
@@ -0,0 +1,7 @@
+package io.github.seoj17.presentaion.ui.champion.detail
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class ChampionDetailViewModel @Inject constructor() : ViewModel() {
+}
