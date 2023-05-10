7ed4adf6836553089588da735028fecb052e3c7d
SEO-J17 <qw4643987@naver.com>
2023-05-11 04:08:29 +0900

    #53 :hammer: livedata 블록으로 코드 수정함

--- presentation/src/main/java/io/github/seoj17/presentaion/ui/champion/detail/ChampionDetailViewModel.kt
+++ presentation/src/main/java/io/github/seoj17/presentaion/ui/champion/detail/ChampionDetailViewModel.kt
@@ -1,14 +1,11 @@
 package io.github.seoj17.presentaion.ui.champion.detail
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.liveData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.github.seoj17.domain.usecase.champion.GetChampionDetailUseCase
 import io.github.seoj17.presentaion.model.Champion
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -20,14 +17,9 @@ class ChampionDetailViewModel @Inject constructor(
     private val championKey =
         ChampionDetailFragmentArgs.fromSavedStateHandle(savedStateHandle).champKey
 
-    private val _champDetail = MutableLiveData<Champion>()
-    val champDetail: LiveData<Champion> = _champDetail
-
-    init {
-        viewModelScope.launch {
-            getChampionDetailUseCase(championKey)?.let {
-                _champDetail.value = Champion(it)
-            }
+    val champDetail = liveData {
+        getChampionDetailUseCase(championKey)?.let {
+            emit(Champion(it))
         }
     }
 }
