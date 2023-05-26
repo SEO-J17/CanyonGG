84a6cffdba1d1c5f821120cb22e1d7014bf9defc
SEO-J17 <qw4643987@naver.com>
2023-05-26 22:27:09 +0900

    #51 :hammer: 챔피언 북마크 추가,삭제 함수 구현함

--- presentation/src/main/java/io/github/seoj17/presentaion/ui/champion/ChampionViewModel.kt
+++ presentation/src/main/java/io/github/seoj17/presentaion/ui/champion/ChampionViewModel.kt
@@ -1,21 +1,38 @@
 package io.github.seoj17.presentaion.ui.champion
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.liveData
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.github.seoj17.domain.usecase.bookmark.AddBookmarkChampionUseCase
+import io.github.seoj17.domain.usecase.bookmark.DeleteBookmarkChampionUseCase
 import io.github.seoj17.domain.usecase.champion.GetAllChampionUseCase
 import io.github.seoj17.presentaion.model.Champion
+import io.github.seoj17.presentaion.model.ChampionBookmark
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ChampionViewModel @Inject constructor(
-    private val getAllChampionUseCase: GetAllChampionUseCase,
+    getAllChampionUseCase: GetAllChampionUseCase,
+    private val addBookmarkChampionUseCase: AddBookmarkChampionUseCase,
+    private val deleteBookmarkChampionUseCase: DeleteBookmarkChampionUseCase,
 ) : ViewModel() {
 
-    val champion: LiveData<List<Champion>> = liveData {
-        emit(
-            getAllChampionUseCase().map { Champion(it) },
-        )
+    val champion = getAllChampionUseCase().map {
+        Champion(it)
+    }.asLiveData()
+
+    fun addBookmark(champion: Champion) {
+        viewModelScope.launch {
+            addBookmarkChampionUseCase(ChampionBookmark.toDomain(champion))
+        }
+    }
+
+    fun deleteBookmark(champion: Champion) {
+        viewModelScope.launch {
+            deleteBookmarkChampionUseCase(champion.key)
+        }
     }
 }
