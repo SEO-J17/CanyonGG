3c51447be7abd522cf7044c849e5979d23bff356
SEO-J17 <qw4643987@naver.com>
2023-05-26 22:26:36 +0900

    #51 :hammer: 챔피언 리스트 북마크 추가,삭제 클릭리스너 구현함

--- presentation/src/main/java/io/github/seoj17/presentaion/ui/champion/ChampionListAdapter.kt
+++ presentation/src/main/java/io/github/seoj17/presentaion/ui/champion/ChampionListAdapter.kt
@@ -6,13 +6,15 @@ import io.github.seoj17.presentaion.model.Champion
 
 class ChampionListAdapter(
     private val onChampionClick: (Int) -> Unit,
+    private val onAddBookmarkClick: (Champion) -> Unit,
+    private val onDeleteBookmarkClick: (Champion) -> Unit,
 ) : ListAdapter<Champion, ChampionViewHolder>(Champion.diffUtil) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ChampionViewHolder(parent)
 
     override fun onBindViewHolder(holder: ChampionViewHolder, position: Int) {
         val data = getItem(position)
         with(holder) {
-            bind(data)
+            bind(data, onAddBookmarkClick, onDeleteBookmarkClick)
             itemView.setOnClickListener {
                 onChampionClick(data.key)
             }
