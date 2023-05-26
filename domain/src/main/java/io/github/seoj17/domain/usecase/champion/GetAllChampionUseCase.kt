4b24853405673907d5adbcf3816c5f2b465ee335
SEO-J17 <qw4643987@naver.com>
2023-05-26 22:24:16 +0900

    #51 :hammer: 북마크 상태 추가하여 flow로 반환타입 변경함

--- domain/src/main/java/io/github/seoj17/domain/usecase/champion/GetAllChampionUseCase.kt
+++ domain/src/main/java/io/github/seoj17/domain/usecase/champion/GetAllChampionUseCase.kt
@@ -3,17 +3,28 @@ package io.github.seoj17.domain.usecase.champion
 import dagger.Reusable
 import io.github.seoj17.data.repository.champion.ChampionsRepository
 import io.github.seoj17.domain.model.ChampionsDomainModel
+import io.github.seoj17.domain.usecase.bookmark.GetBookmarkChampionUseCase
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 @Reusable
 class GetAllChampionUseCase @Inject constructor(
     private val championsRepository: ChampionsRepository,
+    private val getBookmarkChampionUseCase: GetBookmarkChampionUseCase,
 ) {
-    suspend operator fun invoke(): List<ChampionsDomainModel> {
-        return championsRepository
-            .getAllChampion()
-            .map {
-                ChampionsDomainModel(it)
+    operator fun invoke(): Flow<List<ChampionsDomainModel>> {
+        return flow {
+            val champList = championsRepository.getAllChampion()
+
+            getBookmarkChampionUseCase().collect { bookmarkChampList ->
+                emit(
+                    champList.map { champ ->
+                        val isBookmark = bookmarkChampList.any { it.key == champ.key }
+                        ChampionsDomainModel(champ, isBookmark)
+                    },
+                )
             }
+        }
     }
 }
