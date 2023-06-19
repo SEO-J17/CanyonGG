41d084437a87519b9e7413331f8ae48ff83c9782
SEO-J17 <qw4643987@naver.com>
2023-06-19 17:31:52 +0900

    #63 :hammer: 리사이클러 blink off, 북마크 이벤트 추가

--- presentation/src/main/java/io/github/seoj17/presentaion/ui/champion/ChampionFragment.kt
+++ presentation/src/main/java/io/github/seoj17/presentaion/ui/champion/ChampionFragment.kt
@@ -1,6 +1,9 @@
 package io.github.seoj17.presentaion.ui.champion
 
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import io.github.seoj17.presentaion.BR
@@ -8,6 +11,7 @@ import io.github.seoj17.presentaion.R
 import io.github.seoj17.presentaion.databinding.FragmentChampionBinding
 import io.github.seoj17.presentaion.ui.base.BaseDataBindingFragment
 import io.github.seoj17.presentaion.utils.showToast
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class ChampionFragment : BaseDataBindingFragment<FragmentChampionBinding, ChampionViewModel>(
@@ -18,24 +22,31 @@ class ChampionFragment : BaseDataBindingFragment<FragmentChampionBinding, Champi
 
     override fun bindLayout() {
         with(binding) {
+            championList.itemAnimator = null
             championList.adapter = ChampionListAdapter(
                 { champKey ->
                     findNavController().navigate(
                         ChampionFragmentDirections.actionChampionsToChampionDetailFragment(champKey),
                     )
                 },
-                { addBookmarkChamp ->
-                    viewModel.addBookmark(addBookmarkChamp)
-                    requireActivity().showToast(R.string.add_bookmark_message)
-                },
-                { deleteBookmarkChamp ->
-                    viewModel.deleteBookmark(deleteBookmarkChamp)
-                    requireActivity().showToast(R.string.delete_bookmark_message)
+                { champion ->
+                    viewModel.onBookmarkClick(champion)
                 },
             )
         }
     }
 
     override fun observeViewModel() {
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                viewModel.bookmarkEvent.collect {
+                    if (it) {
+                        requireActivity().showToast(R.string.add_bookmark_message)
+                    } else {
+                        requireActivity().showToast(R.string.delete_bookmark_message)
+                    }
+                }
+            }
+        }
     }
 }
