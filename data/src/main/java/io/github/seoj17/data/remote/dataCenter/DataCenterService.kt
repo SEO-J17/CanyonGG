a9adabe79c068782322a781569db393433fc0100
SEO-J17 <qw4643987@naver.com>
2024-05-15 21:24:15 +0900

    :hammer: #89 suspend 방식으로 코드 수정

--- data/src/main/java/io/github/seoj17/data/remote/dataCenter/DataCenterService.kt
+++ data/src/main/java/io/github/seoj17/data/remote/dataCenter/DataCenterService.kt
@@ -3,48 +3,14 @@ package io.github.seoj17.data.remote.dataCenter
 import io.github.seoj17.data.remote.response.champion.ChampResponse
 import io.github.seoj17.data.remote.response.perks.PerksResponse
 import io.github.seoj17.data.remote.response.spell.SpellResponse
-import retrofit2.await
 import javax.inject.Inject
 
 class DataCenterService @Inject constructor(
     private val dataCenterApi: DataCenterApi,
 ) {
-    suspend fun getSpells(): SpellResponse {
-        return runCatching {
-            dataCenterApi.getSpellInfo().await()
-        }.fold(
-            onSuccess = { spellInfo ->
-                spellInfo
-            },
-            onFailure = {
-                throw it
-            },
-        )
-    }
-
-    suspend fun getPerks(): List<PerksResponse> {
-        return runCatching {
-            dataCenterApi.getPerksInfo().await()
-        }.fold(
-            onSuccess = { perks ->
-                perks
-            },
-            onFailure = {
-                emptyList()
-            },
-        )
-    }
-
-    suspend fun getChamps(): ChampResponse {
-        return runCatching {
-            dataCenterApi.getChampInfo().await()
-        }.fold(
-            onSuccess = { champs ->
-                champs
-            },
-            onFailure = {
-                throw it
-            },
-        )
-    }
+    suspend fun getSpells(): SpellResponse = dataCenterApi.getSpellInfo()
+    
+    suspend fun getPerks(): List<PerksResponse> = dataCenterApi.getPerksInfo()
+    
+    suspend fun getChamps(): ChampResponse = dataCenterApi.getChampInfo()
 }
--- data/src/main/java/io/github/seoj17/data/remote/match/MatchesService.kt
+++ data/src/main/java/io/github/seoj17/data/remote/match/MatchesService.kt
@@ -1,35 +1,14 @@
 package io.github.seoj17.data.remote.match
 
 import io.github.seoj17.data.remote.response.match.MatchInfoResponse
-import retrofit2.await
 import javax.inject.Inject
 
 class MatchesService @Inject constructor(
     private val matchesApi: MatchesApi,
 ) {
-    suspend fun getMatchId(puuid: String, start: Int): List<String> {
-        return runCatching {
-            matchesApi.getMatchId(puuid, start).await()
-        }.fold(
-            onSuccess = { matchesId ->
-                matchesId
-            },
-            onFailure = {
-                emptyList()
-            },
-        )
-    }
-
-    suspend fun getMatchInfo(matchId: String): MatchInfoResponse {
-        return runCatching {
-            matchesApi.getMatchInfo(matchId).await()
-        }.fold(
-            onSuccess = { matchInfo ->
-                matchInfo
-            },
-            onFailure = {
-                throw it
-            },
-        )
-    }
+    suspend fun getMatchId(puuid: String, start: Int): List<String> =
+        matchesApi.getMatchId(puuid, start)
+    
+    suspend fun getMatchInfo(matchId: String): MatchInfoResponse =
+        matchesApi.getMatchInfo(matchId)
 }
